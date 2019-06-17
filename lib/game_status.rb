091f7cea55882f88cb7fac32972c0c344e3513b5
S Yoon <sohizzhang@gmail.com>
2019-06-17 13:42:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,13 +25,11 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
   end
-
-  if position_1 == position_2 && position_2 == position_3 && position_taken?(board, index)
+  
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
     puts numbers
-  # if position_1 == "X" && position_2 == "X" && position_3 == "X"
-  #   puts numbers
-  # elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-  #   puts numbers
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    puts numbers
   else
     false
   end
